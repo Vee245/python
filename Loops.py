9659c41bf3784e9797fcf9475dee174b8956e97a
Vee <violetkui05@gmail.com>
2021-11-12 09:44:42 +0300

    second commit

---
+++ Loops.py
@@ -0,0 +1,25 @@
+#While loop
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+
+print ("Good bye!")
+
+
+
+#using while with else
+count = 0
+while count < 5:
+   print (count, " is  less than 5")
+   count = count + 1
+else:
+   print (count, " is not less than 5")
+
+   #infinite loop
+var = 1
+while var == 1 :  # This constructs an infinite loop
+   num = input("Enter a number  :")
+   print ("You entered: ", num)
+
+print ("Good bye!")
